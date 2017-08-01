c946502219245a6aeb51cf884908444fe546b10f
Cherine Elfadel <cherineelf@gmail.com>
2017-08-01 17:21:44 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+// effect work project
+$(document).ready(function() {
+
+    $('.thumbnail').hover(
+      function(){
+        $(this).find('.caption').slideDown(260); //.fadeIn(250)
+      },
+      function(){
+        $(this).find('.caption').slideUp(260); //.fadeOut(205)
+      }
+    );
+});
+
+// navbar effect on scroll
+$(document).ready(function(){
+   var scroll_start = 0;
+   var startchange = $('#startchange');
+   var offset = startchange.offset();
+   $(document).scroll(function() {
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top) {
+          $('#navbarscroll').css('background-color', 'rgba(71,71,71, 0.3)');
+       } else {
+          $('#navbarscroll').css('background-color', 'transparent');
+       }
+   });
+});
