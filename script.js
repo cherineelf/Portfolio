d2408ea9594800f5b74ed60c9a7a06798eda9468
Cherine Elfadel <cherineelf@gmail.com>
2017-08-13 17:18:07 +0200

    work section + modif on jquery / navbar color

--- script.js
+++ script.js
@@ -3,25 +3,25 @@ $(document).ready(function() {
 
     $('.thumbnail').hover(
       function(){
-        $(this).find('.caption').slideDown(260); //.fadeIn(250)
+        $(this).find('.caption').slideDown('slow'); //.fadeIn(250)
       },
       function(){
-        $(this).find('.caption').slideUp(260); //.fadeOut(205)
+        $(this).find('.caption').slideUp('slow'); //.fadeOut(205)
       }
     );
 });
 
 // navbar effect on scroll
-$(document).ready(function(){
-   var scroll_start = 0;
-   var startchange = $('#startchange');
-   var offset = startchange.offset();
-   $(document).scroll(function() {
-      scroll_start = $(this).scrollTop();
-      if(scroll_start > offset.top) {
-          $('#navbarscroll').css('background-color', 'rgba(71,71,71, 0.3)');
-       } else {
-          $('#navbarscroll').css('background-color', 'transparent');
-       }
-   });
-});
+// $(document).ready(function(){
+//    var scroll_start = 0;
+//    var startchange = $('#startchange');
+//    var offset = startchange.offset();
+//    $(document).scroll(function() {
+//       scroll_start = $(this).scrollTop();
+//       if(scroll_start > offset.top) {
+//           $('#navbarscroll').css('background-color', 'rgba(71,71,71, 0.3)');
+//        } else {
+//           $('#navbarscroll').css('background-color', 'transparent');
+//        }
+//    });
+// });
