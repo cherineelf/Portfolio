9c43776f893a0c8c55d10d4f7dd502afa1e243d1
Cherine Elfadel <cherineelf@gmail.com>
2017-08-18 17:41:23 +0200

    link ok

--- script.js
+++ script.js
@@ -10,18 +10,3 @@ $(document).ready(function() {
       }
     );
 });
-
-// navbar effect on scroll
-// $(document).ready(function(){
-//    var scroll_start = 0;
-//    var startchange = $('#startchange');
-//    var offset = startchange.offset();
-//    $(document).scroll(function() {
-//       scroll_start = $(this).scrollTop();
-//       if(scroll_start > offset.top) {
-//           $('#navbarscroll').css('background-color', 'rgba(71,71,71, 0.3)');
-//        } else {
-//           $('#navbarscroll').css('background-color', 'transparent');
-//        }
-//    });
-// });
