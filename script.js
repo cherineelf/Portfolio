bc53ee778fa201e23f9c3284050a0ced5809340d
Cherine Elfadel <cherineelf@gmail.com>
2017-12-12 23:39:20 +0100

    section project almost done

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-// effect button banner
+// effect on button banner //
 
 $('.btn').hover(function(){
   $(this).html("Connect");
 }, function(){
   $(this).html("Hello");
 });
+
+// effect on project hover //
+// $(".hover").mouseleave(
+//   function () {
+//     $(this).removeClass("hover");
+//   }
+// );
