6c97b98cc81bd922a0598185860e089dc37f05c5
Cherine Elfadel <cherineelf@gmail.com>
2018-07-01 16:37:44 +0200

    changed main pic + design h2 about

--- script.js
+++ script.js
@@ -28,4 +28,21 @@ $(document).ready(function(){
 
 $('.btn').click(function () {
   $('body').toggleClass('l-active');
+
 });
+
+
+// fading background
+$(document).on("scroll", function() {
+  var scrollTop = $(document).scrollTop()
+  var scrollBottom = scrollTop + $(window).height()
+
+  var pageBottom = $(document).height()
+
+  var diff =  pageBottom - scrollBottom
+
+  var opacity = 1 - diff / 300
+
+  $("div.fade-bg").css("opacity", opacity)
+
+})
